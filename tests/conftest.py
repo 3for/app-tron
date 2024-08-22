ef94e25c774a0dbba16f17e65c1f3e792c5c4da8
Inuyasha <Inuyasha@local>
2024-08-22 11:00:35 +0800

    feat: delete unused code

--- tests/conftest.py
+++ tests/conftest.py
@@ -77,7 +77,6 @@ def configuration(backend: BackendInterface, navigator, firmware):
             if firmware.device != "nanos":
                 # Skip two step
                 instructions+=[
-                    NavInsID.RIGHT_CLICK,
                     NavInsID.RIGHT_CLICK
                 ]
             instructions+=[
