9d39b699bb5629e4c0bb9f6267857cc9d7e77496
3for <287494524@qq.com>
2025-05-19 22:41:11 +0800

    remove Stax trusted_name part1 and part2

--- src/ui/ui_trusted_name_bagl.h
+++ src/ui/ui_trusted_name_bagl.h
@@ -1,14 +1,14 @@
+#ifdef HAVE_BAGL
 #ifdef HAVE_TRUSTED_NAME
 
 #ifndef UI_TRUSTED_NAME_H_
 #define UI_TRUSTED_NAME_H_
 
 #include "ux.h"
-#ifdef HAVE_BAGL
 
 extern const ux_flow_step_t ux_trusted_name_step;
 
-#endif
 #endif  // UI_TRUSTED_NAME_H_
 
 #endif  // HAVE_TRUSTED_NAME
+#endif  //HAVE_BAGL
