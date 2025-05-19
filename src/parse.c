f21c70f85db9f65510816a66c9b0fc627a775ec0
3for <287494524@qq.com>
2025-05-19 13:20:55 +0800

    fix test_trx_send_asset_with_name

--- src/parse.c
+++ src/parse.c
@@ -818,10 +818,10 @@ parserStatus_e processTx(uint8_t *buffer, uint32_t length, txContent_t *content)
             default:
                 return USTREAM_FAULT;
         }
-        return ret ? USTREAM_FINISHED : USTREAM_FAULT;
+        return ret ? USTREAM_PROCESSING : USTREAM_FAULT;
     }
 
-    return USTREAM_FINISHED;
+    return USTREAM_PROCESSING;
 }
 
 int bytes_to_string(char *out, size_t outl, const void *value, size_t len) {
