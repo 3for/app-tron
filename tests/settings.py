eeda3303a590dd2bd9df9cb901cb95307c7e2cd6
3for <287494524@qq.com>
2025-06-03 17:53:41 +0800

    add setting tests for Stax

--- tests/settings.py
+++ tests/settings.py
@@ -1,89 +1,84 @@
 from enum import Enum, auto
-from ragger.firmware import Firmware
-from ragger.navigator import Navigator, NavInsID, NavIns
 from typing import Union
+from ledgered.devices import Device, DeviceType
 
+from ragger.navigator import Navigator, NavInsID, NavIns
 
+# The order must match that of BAGL and NBGL.
 class SettingID(Enum):
-    pass
-
-
-class NanoSettingID(SettingID):
-    FLOW_1 = auto()
-    FLOW_2 = auto()
-    FLOW_3 = auto()
-    FLOW_4 = auto()
-    VERBOSE_ENS = auto()
+    DATA_ALLOWED = auto()
+    CUSTOM_CONTRACT = auto()
+    SIGN_BY_HASH = auto()
     VERBOSE_TIP712 = auto()
+    TRUSTED_NAME = auto()
 
 
-class NonNanoSettingID(SettingID):
-    TX_DATA_ID = auto()
-    CSTM_CONTRACTS_ID = auto()
-    HASH_TX_ID = auto()
-    VERBOSE_ENS = auto()
-    VERBOSE_TIP712 = auto()
+# Settings Positions per device. Returns the tuple (page, x, y)
+SETTINGS_POSITIONS = {
+    DeviceType.STAX: {
+        SettingID.DATA_ALLOWED: (0, 350, 130),
+        SettingID.CUSTOM_CONTRACT: (0, 350, 270),
+        SettingID.SIGN_BY_HASH: (0, 350, 430),
+        SettingID.VERBOSE_TIP712: (1, 350, 130),
+        SettingID.TRUSTED_NAME: (1, 350, 270),
+    },
+    DeviceType.FLEX: {
+        SettingID.DATA_ALLOWED: (0, 420, 130),
+        SettingID.CUSTOM_CONTRACT: (0, 420, 350),
+        SettingID.SIGN_BY_HASH: (1, 420, 130),
+        SettingID.VERBOSE_TIP712: (1, 420, 270),
+        SettingID.TRUSTED_NAME: (2, 420, 140),
+    },
+}
 
 
-def get_device_settings(firmware: Firmware) -> list:
-    if firmware.is_nano:
-        if firmware == Firmware.NANOS:
-            return [
-                NanoSettingID.NONCE,
-                NanoSettingID.DEBUG_DATA,
-            ]
+# The order of the settings is important, as it is used to navigate
+def get_device_settings(device: Device) -> list[SettingID]:
+    """Get the list of settings available on the device"""
+    if device.is_nano:
         return [
-            NanoSettingID.FLOW_1,
-            NanoSettingID.FLOW_2,
-            NanoSettingID.FLOW_3,
-            NanoSettingID.FLOW_4,
-            NanoSettingID.VERBOSE_ENS,
-            NanoSettingID.VERBOSE_TIP712,
+            SettingID.DATA_ALLOWED,
+            SettingID.CUSTOM_CONTRACT,
+            SettingID.SIGN_BY_HASH,
+            SettingID.VERBOSE_TIP712,
+            SettingID.TRUSTED_NAME,
         ]
-    else:
-        return [
-            NonNanoSettingID.TX_DATA_ID,
-            NonNanoSettingID.CSTM_CONTRACTS_ID,
-            NonNanoSettingID.HASH_TX_ID,
-            NonNanoSettingID.VERBOSE_ENS,
-            NonNanoSettingID.VERBOSE_TIP712,
-        ]
+    return [
+        SettingID.DATA_ALLOWED,
+        SettingID.CUSTOM_CONTRACT,
+        SettingID.SIGN_BY_HASH,
+        SettingID.VERBOSE_TIP712,
+        SettingID.TRUSTED_NAME,
+    ]
 
 
-# Maintain the status quo, no need to be consistent with Ethereum.
-def get_setting_position(
-        firmware: Firmware, setting: Union[NavInsID,
-                                           SettingID]) -> tuple[int, int]:
-    settings_per_page = 3 if firmware == Firmware.STAX else 2
-    y_index = get_device_settings(firmware).index(
-        NonNanoSettingID(setting)) % settings_per_page
-    return 200, 150 * (y_index + 1)
-
-
-def settings_toggle(firmware: Firmware, nav: Navigator,
-                    to_toggle: list[SettingID]):
+def get_settings_moves(device: Device,
+                       to_toggle: list[SettingID]) -> list[Union[NavIns, NavInsID]]:
+    """Get the navigation instructions to toggle the settings"""
     moves: list[Union[NavIns, NavInsID]] = []
-    settings = get_device_settings(firmware)
-    # Assume the app is on the home page
-    if firmware.is_nano:
-        moves += [NavInsID.RIGHT_CLICK] * 2
-        moves += [NavInsID.BOTH_CLICK]
+    settings = get_device_settings(device)
+    # Assume the app is on the 1st page of Settings
+    if device.is_nano:
+        moves += [NavInsID.RIGHT_CLICK, NavInsID.BOTH_CLICK]
         for setting in settings:
             if setting in to_toggle:
                 moves += [NavInsID.BOTH_CLICK]
             moves += [NavInsID.RIGHT_CLICK]
         moves += [NavInsID.BOTH_CLICK]  # Back
     else:
+        current_page = 0
         moves += [NavInsID.USE_CASE_HOME_SETTINGS]
-        settings_per_page = 3 if firmware == Firmware.STAX else 2
         for setting in settings:
-            setting_idx = settings.index(setting)
-            if (setting_idx > 0) and (setting_idx % settings_per_page) == 0:
-                moves += [NavInsID.USE_CASE_SETTINGS_NEXT]
             if setting in to_toggle:
-                moves += [
-                    NavIns(NavInsID.TOUCH,
-                           get_setting_position(firmware, setting))
-                ]
+                page, x, y = SETTINGS_POSITIONS[device.type][setting]
+                moves += [NavInsID.USE_CASE_SETTINGS_NEXT] * (page - current_page)
+                moves += [NavIns(NavInsID.TOUCH, (x, y))]
+                current_page = page
         moves += [NavInsID.USE_CASE_SETTINGS_MULTI_PAGE_EXIT]
-    nav.navigate(moves, screen_change_before_first_instruction=False)
+    return moves
+
+
+def settings_toggle(device: Device, navigator: Navigator, to_toggle: list[SettingID]):
+    """Toggle the settings"""
+    moves = get_settings_moves(device, to_toggle)
+    navigator.navigate(moves, screen_change_before_first_instruction=False)
