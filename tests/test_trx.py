6ed59d55e759d1bb214646341adb19fde07c2c1d
3for <287494524@qq.com>
2025-06-04 22:37:46 +0800

    fix tip712 tests for Stax

--- tests/test_trx.py
+++ tests/test_trx.py
@@ -39,6 +39,7 @@ from dataset import DataSet, ADVANCED_DATA_SETS, TOKENS, TRUSTED_NAMES, FILT_TN_
 from utils import recover_message
 from web3 import Web3
 from ledgered.devices import Device
+from ragger.firmware.touch.positions import POSITIONS
 '''
 Tron Protobuf
 '''
