256052b9801b5ab8fef06ba10942b4fc9c67339c
3for <287494524@qq.com>
2025-07-28 16:13:43 +0800

    fix macro definition

--- src/ledger_pki.c
+++ src/ledger_pki.c
@@ -1,4 +1,3 @@
-// #include "apdu_constants.h"
 #include "public_keys.h"
 #include <stdint.h>
 
@@ -15,6 +14,9 @@
      : x == CERTIFICATE_PUBLIC_KEY_USAGE_PLUGIN_METADATA      ? "PLUGIN_METADATA"      \
      : x == CERTIFICATE_PUBLIC_KEY_USAGE_COIN_META            ? "COIN_META"            \
      : x == CERTIFICATE_PUBLIC_KEY_USAGE_SEED_ID_AUTH         ? "SEED_ID_AUTH"         \
+     : x == CERTIFICATE_PUBLIC_KEY_USAGE_TX_SIMU_SIGNER       ? "TX_SIMU_SIGNER"       \
+     : x == CERTIFICATE_PUBLIC_KEY_USAGE_CALLDATA             ? "CALLDATA"             \
+     : x == CERTIFICATE_PUBLIC_KEY_USAGE_NETWORK              ? "NETWORK"              \
                                                               : "Unknown")
 
 int check_signature_with_pubkey(const char *tag,
@@ -22,26 +24,21 @@ int check_signature_with_pubkey(const char *tag,
                                 const uint8_t bufLen,
                                 const uint8_t *PubKey,
                                 const uint8_t keyLen,
-#ifdef HAVE_LEDGER_PKI
                                 const uint8_t keyUsageExp,
-#endif
                                 uint8_t *signature,
                                 const uint8_t sigLen) {
     UNUSED(tag);
     cx_ecfp_public_key_t verif_key = {0};
     cx_err_t error = CX_INTERNAL_ERROR;
-#ifdef HAVE_LEDGER_PKI
     uint8_t key_usage = 0;
     size_t trusted_name_len = 0;
     uint8_t trusted_name[CERTIFICATE_TRUSTED_NAME_MAXLEN] = {0};
     cx_ecfp_384_public_key_t public_key = {0};
-#endif
 
     PRINTF(
         "[%s] "
         "=======================================================================================\n",
         tag);
-#ifdef HAVE_LEDGER_PKI
     error = os_pki_get_info(&key_usage, trusted_name, &trusted_name_len, &public_key);
     if ((error == 0) && (key_usage == keyUsageExp)) {
         PRINTF("[%s] Certificate '%s' loaded for usage 0x%x (%s)\n",
@@ -58,24 +55,9 @@ int check_signature_with_pubkey(const char *tag,
             goto end;
 #endif
         }
-    } else
-#endif
-    {
+    } else {
         PRINTF("[%s] ********** No certificate loaded. Using legacy path **********\n", tag);
         CX_CHECK(cx_ecfp_init_public_key_no_throw(CX_CURVE_256K1, PubKey, keyLen, &verif_key));
-        PRINTF("params %d %.*H %.*H %.*H\n",
-               verif_key.curve,
-               verif_key.W_len,
-               verif_key.W,
-               bufLen,
-               buffer,
-               sigLen,
-               signature);
-        //  const cx_ecfp_public_key_t *pukey,
-        //  const uint8_t              *hash,
-        //  size_t                      hash_len,
-        //  const uint8_t              *sig,
-        //  size_t                      sig_len
         if (!cx_ecdsa_verify_no_throw(&verif_key, buffer, bufLen, signature, sigLen)) {
             PRINTF("%s: Invalid signature\n", tag);
 #ifndef HAVE_BYPASS_SIGNATURES
