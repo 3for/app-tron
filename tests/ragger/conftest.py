48dac846e675ace6760ab35769a34e9d15ceb9b9
3for <287494524@qq.com>
2026-03-16 19:47:23 +0800

    refactor ragger test layout and fix CI test paths

---
+++ tests/ragger/conftest.py
@@ -0,0 +1,106 @@
+# This final fixture will return the properly configured backend client, to be used in tests
+import pytest
+from ragger.conftest import configuration
+from ragger.backend import SpeculosBackend, BackendInterface
+from ragger.navigator import NavInsID, NavIns
+from pathlib import Path
+import re
+
+###########################
+### CONFIGURATION START ###
+###########################
+MNEMONIC = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+
+configuration.OPTIONAL.BACKEND_SCOPE = "class"
+configuration.OPTIONAL.CUSTOM_SEED = MNEMONIC
+
+
+@pytest.fixture(scope="class")
+def configuration(backend: BackendInterface, navigator, firmware):
+    if type(backend) is SpeculosBackend:
+        if firmware.device == "flex":
+            instructions = [
+                # Go to settings menu.
+                NavIns(NavInsID.USE_CASE_HOME_SETTINGS),
+                # Allow data in TXs
+                NavIns(NavInsID.TOUCH, (200, 150)),
+                # Allow custom contracts
+                NavIns(NavInsID.TOUCH, (200, 300)),
+                NavIns(NavInsID.USE_CASE_SETTINGS_NEXT),
+                # Allow sign by hash
+                NavIns(NavInsID.TOUCH, (200, 150)),
+
+                # Go back to main menu.
+                NavIns(NavInsID.USE_CASE_SETTINGS_MULTI_PAGE_EXIT),
+            ]
+        elif firmware.device == "stax":
+            instructions = [
+                # Go to settings menu.
+                NavIns(NavInsID.USE_CASE_HOME_SETTINGS),
+                # Allow data in TXs
+                NavIns(NavInsID.TOUCH, (200, 150)),
+                # Allow custom contracts
+                NavIns(NavInsID.TOUCH, (200, 300)),
+                # Allow sign by hash
+                NavIns(NavInsID.TOUCH, (200, 450)),
+                # Go back to main menu.
+                NavIns(NavInsID.USE_CASE_SETTINGS_MULTI_PAGE_EXIT),
+            ]
+        elif firmware.device.startswith("apex_p"):
+            instructions = [
+                # Go to settings menu.
+                NavIns(NavInsID.USE_CASE_HOME_SETTINGS),
+                # Allow data in TXs
+                NavIns(NavInsID.TOUCH, (150, 110)),
+                # Allow custom contracts
+                NavIns(NavInsID.TOUCH, (150, 220)),
+                NavIns(NavInsID.USE_CASE_SETTINGS_NEXT),
+                # Allow sign by hash
+                NavIns(NavInsID.TOUCH, (150, 110)),
+                # Go back to main menu.
+                NavIns(NavInsID.USE_CASE_SETTINGS_MULTI_PAGE_EXIT),
+            ]
+        else:
+            instructions = [
+                # Go to settings main menu
+                NavInsID.RIGHT_CLICK,
+                NavInsID.RIGHT_CLICK,
+                NavInsID.BOTH_CLICK,
+                # Allow data in TXs
+                NavInsID.BOTH_CLICK,
+                # Allow custom contracts
+                NavInsID.RIGHT_CLICK,
+                NavInsID.BOTH_CLICK,
+                # Allow sign by hash
+                NavInsID.RIGHT_CLICK,
+                NavInsID.RIGHT_CLICK,
+                NavInsID.BOTH_CLICK,
+            ]
+
+            # Skip one step
+            instructions += [NavInsID.RIGHT_CLICK]
+            instructions += [
+                # Go back to main menu
+                NavInsID.RIGHT_CLICK,
+                NavInsID.BOTH_CLICK
+            ]
+
+        navigator.navigate(instructions,
+                           screen_change_before_first_instruction=False)
+
+
+@pytest.fixture(name="app_version")
+def app_version_fixture(request) -> tuple[int, int, int]:
+    with open(Path(__file__).parent.parent.parent / "VERSION", encoding="utf-8") as f:
+        parsed = {}
+        first_line = f.readline().strip()
+        parsed = [int(part) for part in first_line.split('.')]
+    return (parsed[0], parsed[1], parsed[2])
+
+
+#########################
+### CONFIGURATION END ###
+#########################
+
+# Pull all features from the base ragger conftest using the overridden configuration
+pytest_plugins = ("ragger.conftest.base_conftest", )
